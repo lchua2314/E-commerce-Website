c0f20d2397d8f6733dbc3c65832df7408c44781f
lchua2314 <lchua2314@gmail.com>
2020-02-14 22:30:05 -0800

    Changed files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-/*alert("PLEASE DO NOT CLOSE ME YET.\n"
-+ "WELCOME BACK!\n"
-+ "WOULD YOU LIKE TO SUBSCRIBE FOR NEWSLETTERS?\n" 
-+ "PLEASE DO, WE HAVE NO MONEY.");*/
+const sendBtn = document.querySelector('#sendBtn');
+const messageIn = document.querySelector('#messageIn');
+const messageOut = document.querySelector('#messageOut');
+
+sendBtn.addEventListener('click', sendMsg);
+
+function sendMsg () {
+    let content = messageIn.value;
+    //console.log(content);
+    if (content === '') {
+        alert('Please Enter Valid Value. Current Value is Empty.');
+    }
+    else {
+        messageOut.innerHTML = content;
+        messageIn.value = '';
+    }
+}
