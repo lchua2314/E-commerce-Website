1a9b53ed3599faca8d3f25cf163748b4237924dc
lchua2314 <lchua2314@gmail.com>
2020-02-15 15:49:41 -0800

    Added JS that displays username after input

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-const sendBtn = document.querySelector('#sendBtn');
-const messageIn = document.querySelector('#messageIn');
-const messageOut = document.querySelector('#messageOut');
+const sendBtn = document.querySelector('#loginBtn');
+const messageIn = document.querySelector('#userid');
+const messageOut = document.querySelector('#usernameDisplay');
 
 sendBtn.addEventListener('click', sendMsg);
 
 function sendMsg () {
-    let content = messageIn.value;
+    let content = 'Hello, ' + messageIn.value + '!';
     //console.log(content);
     if (content === '') {
         alert('Please Enter Valid Value. Current Value is Empty.');
